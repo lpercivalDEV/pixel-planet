32288e8afd164be73d6273fc5328d81441ad0294
lpercivalDEV <lpercival.dev@gmail.com>
2023-06-26 16:06:36 -0400

    Add responsive sections - placeholders

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
+// GSAP 
+// gsap.to(".hero", {
+//   yPercent: -100,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".hero",
+//     // start: "top bottom", // the default values
+//     // end: "bottom top",
+//     scrub: true
+//   }, 
+// });
+
+// gsap.to("#earf", {
+//   yPercent: 50,
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".hero",
+//     // start: "top bottom", // the default values
+//     // end: "bottom top",
+//     scrub: true
+//   }, 
+// });
+
+
+
+
 // MOBILE NAV BEHAVIOR
